2114e26af21e31461c5210ec30e7f70307dbc6bf
Ammad Shaikh <ammad_shake@yahoo.com>
2017-11-29 12:35:28 -0600

    hello

---
+++ scripts/redditScrape.js
@@ -0,0 +1,106 @@
+// scrape script
+// =============
+
+// Require axios and cheerio, making our scrapes possible
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// This function will scrape the NYTimes website
+var scrape = function() {
+  // Scrape the NYTimes website
+  return axios.get("https://www.reddit.com/r/stockmarket").then(function(res) {
+    var $ = cheerio.load(res.data);
+    // Make an empty array to save our article info
+    var articles = [];
+
+    // Now, find and loop through each element that has the "theme-summary" class
+    // (i.e, the section holding the articles)
+    $(".title").each(function(i, element) {
+      // In each .theme-summary, we grab the child with the class story-heading
+
+      // Then we grab the inner text of the this element and store it
+      // to the head variable. This is the article headline
+      var head = $(this)
+        .children("a")
+        .text()
+        .trim();
+
+      // Grab the URL of the article
+      var url = $(this)
+        .children("a")
+        .attr("href");
+
+      // Then we grab any children with the class of summary and then grab it's inner text
+      // We store this to the sum variable. This is the article summary
+      var sum = $(this)
+        .text()
+        .trim();
+
+      // So long as our headline and sum and url aren't empty or undefined, do the following
+      if (head && sum && url) {
+        // This section uses regular expressions and the trim function to tidy our headlines and summaries
+        // We're removing extra lines, extra spacing, extra tabs, etc.. to increase to typographical cleanliness.
+        var headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+        var sumNeat = sum.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+
+        // Initialize an object we will push to the articles array
+
+        var dataToAdd = {
+          headline: headNeat,
+          summary: sumNeat,
+          url: url
+        };
+
+        articles.push(dataToAdd);
+      }
+    });
+    return articles;
+  });
+};
+
+// Export the function, so other files in our backend can use it
+module.exports = scrape;
+
+
+
+/*var cheerio = require("cheerio");
+var request = require("request");
+
+// First, tell the console what server.js is doing
+console.log("\n***********************************\n" +
+            "Grabbing every thread name and link\n" +
+            "from reddit's stockmarket board:" +
+            "\n***********************************\n");
+
+// Making a request for reddit's "webdev" board. The page's HTML is passed as the callback's third argument
+request("https://www.reddit.com/r/stockmarket", function(error, response, html) {
+
+  // Load the HTML into cheerio and save it to a variable
+  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+  var $ = cheerio.load(html);
+
+  // An empty array to save the data that we'll scrape
+  var results = [];
+
+  // With cheerio, find each p-tag with the "title" class
+  // (i: iterator. element: the current element)
+  $("p.title").each(function(i, element) {
+
+    // Save the text of the element in a "title" variable
+    var title = $(element).text();
+
+    // In the currently selected element, look at its child elements (i.e., its a-tags),
+    // then save the values for any "href" attributes that the child elements may have
+    var link = $(element).children().attr("href");
+
+    // Save these results in an object that we'll push into the results array we defined earlier
+    results.push({
+      title: title,
+      link: link
+    });
+  });
+
+  // Log the results once you've looped through each of the elements found with cheerio
+  console.log(results);
+});
+*/
