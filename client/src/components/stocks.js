fea1affe741efb0b7dfa99481b997890fb6567fb
Brett <brettartmann@gmail.com>
2017-12-02 13:00:22 -0600

    working revisions in helper.js

--- client/src/components/stocks.js
+++ client/src/components/stocks.js
@@ -10,23 +10,23 @@ class Stocks extends Component {
     super(props);
     this.state =  {
         close: '',
-        ticker: ''
+        ticker: []
     }
     this.setQuery = this.setQuery.bind(this);
   }  
 
 savedStocks() {
-    console.log('we hit savedStocks function starting --');
+    // console.log('we hit savedStocks function starting --');
     helpers.getSavedStocks().then((stocks) => {
-      console.log('these are the saved stocks',stocks.data)
-      this.setState({ticker: stocks.data});
+      // console.log('these are the saved stocks',stocks)
+      this.setState({ticker: stocks});
     });
   }
 
  setQuery() {
-    console.log('we hit setQuest function starting --');
+    // console.log('we hit setQuest function starting --');
     helpers.realTimeQuotes().then((data) => {
-      console.log('this is stock price IN THE COMPONENT ready to gooo ---',data)
+      // console.log('this is stock price IN THE COMPONENT ready to gooo ---',data)
       this.setState({close: data});
     });
   }
@@ -40,65 +40,62 @@ savedStocks() {
     render() {
 
 
-    console.log('this is the state of our stock component ------', this.state);
+    
         return (
             <div>
-<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+<nav className="navbar navbar-toggleable-md navbar-light bg-faded">
     <span>
-    <button class="navbar-toggler navbar-toggler-left" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button className="navbar-toggler navbar-toggler-left" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
         <span>
-            <i class="fa fa-bars fa-bars-connection fa-lg" aria-hidden="true"></i>
+            <i className="fa fa-bars fa-bars-connection fa-lg" aria-hidden="true"></i>
         </span>
     </button>
     
 
 
-    <button class="button-clear">
+    <button className="button-clear">
         <a href="/share"> 
-            <i class="fa fa-plus fa-plus-connection fa-lg" aria-hidden="true"></i>
+            <i className="fa fa-plus fa-plus-connection fa-lg" aria-hidden="true"></i>
         </a>
     </button> 
 </span>
 
-    <div class="collapse navbar-collapse" id="navbarContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="/stocks"><h4>Stocks</h4></a>
+    <div className="collapse navbar-collapse" id="navbarContent">
+        <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+                <a className="nav-link" href="/stocks"><h4>Stocks</h4></a>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
                 <a class="nav-link" href="/news"><h4>News</h4></a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/wishlist"><h4>WishList</h4></a>
+            <li className="nav-item">
+                <a className="nav-link" href="/wishlist"><h4>WishList</h4></a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/login" id="sign-out-button"><h4>Sign Out</h4></a>
+            <li className="nav-item">
+                <a className="nav-link" href="/login" id="sign-out-button"><h4>Sign Out</h4></a>
             </li>
         </ul>
     </div>
 </nav>
 
 
-<div class="wrapper">
+<div className="wrapper">
   
-        <button class="btn sort-btn btn-lg" id="listNew" href="/connections/chrono">r/StockMarket</button>
-        <button class="btn sort-btn btn-lg" id="listAlpha" href="/connections/alpha">r/Investing</button>
-        <button class="btn sort-btn btn-lg" id="listAlpha" href="/connections/alpha">r/Investing</button>
+        <button className="btn sort-btn btn-lg" id="listNew" href="/connections/chrono">r/StockMarket</button>
+        <button className="btn sort-btn btn-lg" id="listAlpha" href="/connections/alpha">r/Investing</button>
+        <button className="btn sort-btn btn-lg" id="listAlpha" href="/connections/alpha">r/Investing</button>
     </div>
 
-<div class="container-fluid stocks-container">
+<div className="container-fluid stocks-container">
 </div>
 
-        <footer class="footer">
-          <div class="container">
+        <footer className="footer">
+          <div className="container">
             <p>Stocks Scraper</p>
             <h1>{this.state.close}</h1>
-            {
-                this.state.ticker &&
-                this.state.ticker.map( tickerObj => 
-                    <h3>{tickerObj.ticker}</h3>
-                )
-            }
+            <ul>
+            <li>{this.state.ticker}</li>
+            </ul>
           </div>
         </footer>
  </div>
--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -10,12 +10,29 @@ const helpers = {
     getSavedStocks: function() {
     return axios.get("/api/stocks")
       .then(function(results) {
-        console.log("axios results", results);
-
-        return results;
-      });
+        var data = results.data
+        var tickerArray = []
+        // console.log("axios results", results.data);
+        for (var i = 0; i < data.length; i++) {
+           var tickerSymbol = Object.values(data[i]).pop()
+           tickerArray.push(tickerSymbol)
+            // console.log("this is ticker array", tickerArray)
+            for (var j = 0; j < tickerArray.length; j++) {
+              return axios.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="+tickerArray[j]+"&interval=1min&apikey=768XGV67OZVODVUO")
+              .then(function(response) {
+                var stockPrices = response.data['Time Series (1min)'];
+                var stockName = response.data["Meta Data"]["2. Symbol"]
+                console.log("This is stock name", stockName )
+              })
+            }
+        } 
+        return tickerArray;
+      })
+      // .then(function(){
+      //   console.log("say hello", tickerArray)
+      // })
      },
-
+// var test = Object.keys(data).map(function (date) { return [date, data[date]["4. close"]] })
   realTimeQuotes: function() {
 
     // Hardcoded Stocks for testing
