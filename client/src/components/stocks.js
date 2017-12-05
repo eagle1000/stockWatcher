2da5dffc2234446a18d624ca48a3ce6f2b18bd6f
Brett <brettartmann@gmail.com>
2017-12-05 09:44:59 -0600

    setting chart state

--- client/src/components/stocks.js
+++ client/src/components/stocks.js
@@ -1,101 +1,142 @@
-import React, {Component} from "react"
-import {Link} from "react-router";
+import React, { Component } from "react";
+import { Link } from "react-router";
 // Include the helpers for making API calls
 import helpers from "../utils/helpers";
-import Chart from "./Chart.js"
-
+import Chart from "./Chart.js";
 
 class Stocks extends Component {
- constructor(props) {
-    super(props);
-    this.state =  {
-        hotStocks: [],
-        userStocks: []
+    constructor(props) {
+        super(props);
+        this.state = {
+            hotStocks: [],
+            userStocks: [],
+            chartData: {}
+        };
+        this.savedStocks = this.savedStocks.bind(this);
+        this.userSavedStocks = this.userSavedStocks.bind(this);
+        this.getChartData = this.getChartData.bind(this);
     }
-    this.savedStocks = this.savedStocks.bind(this);
-    this.userSavedStocks = this.userSavedStocks.bind(this);
 
-  }  
+    getChartData() {
+        // ajax calls here
+        helpers.getTopStockChartData().then(data => {
+            this.setState({
+                chartData: {
+                    labels: [
+                        "January1",
+                        "February",
+                        "March",
+                        "April",
+                        "May",
+                        "June",
+                        "July"
+                    ],
+                    datasets: [
+                        {
+                            data: [0, 10, 5, 2, 20, 30, 45]
+                        }
+                    ]
+                }
+            });
+        });
+    }
 
-renderHotStocks(){
-    return this.state.hotStocks.map(function(item){
-        return <p key= {item} >{item}</p>
-    })
-}
-savedStocks() {
-    // console.log('we hit savedStocks function starting --');
-    helpers.getSavedStocks().then((stocks) => {
-      // console.log('these are the saved stocks',stocks)
-      this.setState({hotStocks: stocks});
-    });
-  };
+    renderHotStocks() {
+        return this.state.hotStocks.map(function(item) {
+            return <p key={item}>{item}</p>;
+        });
+    }
 
-  userSavedStocks() {
-     // console.log('we hit savedStocks function starting --');
-    helpers.getUserStocks().then((mystocks) => {
-      // console.log('these are the saved stocks',stocks)
-      this.setState({userStocks: mystocks});
-    });
-  }
+      renderUserStocks() {
+        return this.state.userStocks.map(function(item) {
+            return <p key={item}>{item}</p>;
+        });
+    }
 
-  componentWillMount() {
-    this.savedStocks();
-    this.userSavedStocks();
-  }
+    savedStocks() {
+        // console.log('we hit savedStocks function starting --');
+        helpers.getSavedStocks().then(stocks => {
+            // console.log('these are the saved stocks',stocks)
+            this.setState({ hotStocks: stocks });
+        });
+    }
 
-    render() {  
+    userSavedStocks() {
+        // console.log('we hit savedStocks function starting --');
+        helpers.getUserStocks().then(mystocks => {
+            // console.log('these are the saved stocks',stocks)
+            this.setState({ userStocks: mystocks });
+        });
+    }
+
+    componentWillMount() {
+        this.savedStocks();
+        this.userSavedStocks();
+        this.getChartData();
+    }
+
+    render() {
         return (
             <div>
-    
+                <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+                    <div
+                        className="collapse navbar-collapse"
+                        id="navbarContent"
+                    >
+                        <img
+                            src="http://embswarsaw.com/wp-content/uploads/2017/06/logo_StockWatch2.png"
+                            className="navLogo"
+                        />
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <a className="nav-link" href="/stocks">
+                                    <h4>Stocks</h4>
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/news">
+                                    <h4>News</h4>
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a
+                                    className="nav-link"
+                                    href="/login"
+                                    id="sign-out-button"
+                                >
+                                    <h4>Sign Out</h4>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
 
-        <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
-
-                
-                <div className="collapse navbar-collapse" id="navbarContent">
-                <img src= "http://embswarsaw.com/wp-content/uploads/2017/06/logo_StockWatch2.png" className="navLogo"/>
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item">
-                            <a className="nav-link" href="/stocks"><h4>Stocks</h4></a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/news"><h4>News</h4></a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/login" id="sign-out-button"><h4>Sign Out</h4></a>
-                        </li>
-                    </ul>
+                <div className="wrapper">
+                    <button
+                        className="btn sort-btn btn-lg"
+                        id="listNew"
+                        href="/connections/chrono"
+                    >
+                        Hot Stocks
+                    </button>
                 </div>
-            </nav>
 
+                <div className="container-fluid stocks-container">
+                    <div>{this.renderHotStocks()}</div>
 
-<div className="wrapper">
-  
-        <button className="btn sort-btn btn-lg" id="listNew" href="/connections/chrono">Hot Stocks</button>
-    </div>
+                    <Chart chartData={this.state.chartData}/>
 
-<div className="container-fluid stocks-container">
-    <div>{this.renderHotStocks()}</div>
+                    <div>{this.renderUserStocks()}</div>
+                </div>
 
-    
-    <Chart />
-
-    <h2>{this.state.userStocks}</h2>
-</div>
-
-        <footer className="footer">
-          <div className="container">
-            <p>Stocks Scraper</p>
-         
-            
-            
-           
-          </div>
-        </footer>
- </div>
-                    
-
-            )
+                <footer className="footer">
+                    <div className="container">
+                        <p>Stocks Scraper</p>
+                    </div>
+                </footer>
+            </div>
+        );
     }
 }
 
-export default Stocks
+export default Stocks;
--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -22,20 +22,20 @@ const helpers = {
         })
         //pass through the array of ticker symbols into a for loop where every ticker symbol will do a get request to the API to return pricing data
         .then(async (tickerArray) => {
-          console.log("this is ticker array", tickerArray);
+          // console.log("this is ticker array", tickerArray);
           var tickerData = tickerArray["0"];
           let returnedData = []
           for (let j = 0; j < tickerData.length; j++) {
-            console.log("tickerData length", tickerData.length);
+            // console.log("tickerData length", tickerData.length);
             let stockInfo = await axios.get(
               "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" +
                 tickerData[j] +
                 "&interval=1min&apikey=768XGV67OZVODVUO"
             );
+          // console.log("TODO Show chartable version of: ", stockInfo.data["Time Series (1min)"])            
             returnedData.push(stockInfo.data)
-            console.log
-             console.log('this is stock info', stockInfo)
-             console.log('this is returned data', returnedData)
+             // console.log('this is stock info', stockInfo)
+             // console.log('this is returned data', returnedData)
           }
           return returnedData
         })
@@ -46,18 +46,18 @@ const helpers = {
           for (var k = 0; k < returnedData.length; k++) {
             var stockNames = returnedData[k]["Meta Data"]["2. Symbol"];
             stockNamesArray.push(stockNames)
-            console.log("this is stock names array", stockNamesArray)
+            // console.log("this is stock names array", stockNamesArray)
           }
 
           for (var l = 0; l < returnedData.length; l++) {
             var stockPrices = returnedData[l]["Time Series (1min)"];
-            console.log('these are stock prices', stockPrices)      
+            // console.log('these are stock prices', stockPrices)      
               const latestPrice = stockPrices[Object.keys(stockPrices)[0]];
-               console.log('this is latest price', latestPrice)
+               // console.log('this is latest price', latestPrice)
                const realTimePrice = latestPrice["4. close"];
-               console.log('this is real time price',realTimePrice);
+               // console.log('this is real time price',realTimePrice);
                stockPriceArray.push(realTimePrice)
-               console.log('this is stock price array', stockPriceArray)
+               // console.log('this is stock price array', stockPriceArray)
           }
           let finalArray = []
           for(var i = 0; i < stockPriceArray.length; i ++ ){
@@ -69,24 +69,127 @@ const helpers = {
     );
   },
 // Helper Method for rendering stocks tracked by user
-getUserStocks: function() {
 
+  getUserStocks: function() {
     return (
       axios
-        .get("/api/stocks/:id")
+        .get("/api/stocks")
         .then(function(results) {
-          var data = results.data;
+          var userData = results.data;
           // console.log("this is data array", data);
-          var tickerArray = [];
+          var userTickerArray = [];
           //loop over the database array to extract the value which is the string ticker symbol of each stock
-          for (var i = 0; i < data.length; i++) {
-            var tickerSymbol = Object.values(data[i]).pop();
-            tickerArray.push(tickerSymbol);         
+          for (var i = 0; i < userData.length; i++) {
+            var userTickerSymbol = Object.values(userData[i]).pop();
+            userTickerArray.push(userTickerSymbol);         
           }
-           return tickerArray;
+           return userTickerArray;
         })
-      )
+        //pass through the array of ticker symbols into a for loop where every ticker symbol will do a get request to the API to return pricing data
+        .then(async (userTickerArray) => {
+          // console.log("this is ticker array", userTickerArray);
+          var userTickerData = userTickerArray["0"];
+          let userReturnedData = []
+          for (let j = 0; j < userTickerData.length; j++) {
+            // console.log("userTickerData length", userTickerData.length);
+            let userStockInfo = await axios.get(
+              "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" +
+                userTickerData[j] +
+                "&interval=1min&apikey=768XGV67OZVODVUO"
+            );
+            userReturnedData.push(userStockInfo.data)
+             // console.log('this is stock info', userStockInfo)
+             // console.log('this is returned data', userReturnedData)
+          }
+          return userReturnedData
+        })
+        //Extract the final price from each stock and return with stock name in an array to send to src/components/stocks.js
+        .then(function(userReturnedData) {
+          var userStockNamesArray = []
+          var userStockPriceArray = []
+          for (var k = 0; k < userReturnedData.length; k++) {
+            var userStockNames = userReturnedData[k]["Meta Data"]["2. Symbol"];
+            userStockNamesArray.push(userStockNames)
+            // console.log("this is stock names array", userStockNamesArray)
+          }
+
+          for (var l = 0; l < userReturnedData.length; l++) {
+            var userStockPrices = userReturnedData[l]["Time Series (1min)"];
+            // console.log('these are stock prices', userStockPrices)      
+              const userLatestPrice = userStockPrices[Object.keys(userStockPrices)[0]];
+               // console.log('this is latest price', userLatestPrice)
+               const userRealTimePrice = userLatestPrice["4. close"];
+               // console.log('this is real time price',userRealTimePrice);
+               userStockPriceArray.push(userRealTimePrice)
+               // console.log('this is stock price array', userStockPriceArray)
+          }
+          let userFinalArray = []
+          for(var i = 0; i < userStockPriceArray.length; i ++ ){
+            let userStockDetails = userStockNamesArray[i] + " : " + userStockPriceArray[i]
+            userFinalArray.push(userStockDetails)
+            console.log(userFinalArray)
+          }
+          return userFinalArray
+        })
+    );
+  },
+//Return daily chart data for Hot Stocks
+getTopStockChartData: function() {
+    return (
+      axios
+        .get("/api/stocks")
+        .then(function(results) {
+          var chartData = results.data;
+          // console.log("this is data array", data);
+          var chartTickerArray = [];
+          //loop over the database array to extract the value which is the string ticker symbol of each stock
+          for (var i = 0; i < chartData.length; i++) {
+            var chartTickerSymbol = Object.values(chartData[i]).pop();
+            chartTickerArray.push(chartTickerSymbol);         
+          }
+           return chartTickerArray;
+        })
+         //pass through the array of ticker symbols into a for loop where every ticker symbol will do a get request to the API to return pricing data
+        .then(async (chartTickerArray) => {
+          console.log("this is chart ticker array", chartTickerArray);
+          var chartTickerData = chartTickerArray["0"];
+          let chartReturnedData = []
+          for (let j = 0; j < chartTickerData.length; j++) {
+            console.log("chartTickerData length", chartTickerData.length);
+            let chartStockInfo = await axios.get(
+              "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" +
+                chartTickerData[j] +
+                "&interval=1min&apikey=768XGV67OZVODVUO"
+            );            
+            chartReturnedData.push(chartStockInfo.data)
+            //  console.log('this is chart stock info', chartStockInfo)
+             console.log('this is chart returned data', chartReturnedData)
+          }
+          return chartReturnedData
+        })
+        .then(function(chartReturnedData) {
+           var chartDateArray = []
+           var chartPriceArray = []
+
+            for (var k = 0; k < chartReturnedData.length; k++) {
+            var dailyData = chartReturnedData[k]["Time Series (Daily)"];
+            var dailyData2 = Object.keys(dailyData);
+            // console.log("this is daily data2", dailyData2)
+            chartDateArray.push(dailyData2);
+            // console.log("chart date array", chartDateArray)
+            var dailyPrice = chartReturnedData[k]["Time Series (Daily)"]
+            console.log("this is daily price", dailyPrice)
+            var dailyPriceSeries = Object.values(dailyPrice);         
+            console.log("Daily Price Series", dailyPriceSeries);
+             var chartPrice = dailyPriceSeries.map(function (index) { return dailyPriceSeries["4. close"] })
+                console.log('chart prices', chartPrice)
+
+          }
+        })
+
+      )   
 }
+// Helpers closing brace
 };
 
 // We export the helpers function
